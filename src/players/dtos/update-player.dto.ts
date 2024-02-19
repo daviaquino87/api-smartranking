de5c5fd8eb41dbcef4b94ed1666d0ba1eb7e9e6d
daviaquino87 <davi26031@gmail.com@gmail.com>
2024-02-19 09:15:28 -0300

    refactor: add phone in update player dto

--- src/players/dtos/update-player.dto.ts
+++ src/players/dtos/update-player.dto.ts
@@ -1,7 +1,12 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class UpdatePlayerDTO {
   @MinLength(3)
   @IsString()
   name: string;
+
+  @MinLength(11)
+  @MaxLength(19)
+  @IsString()
+  readonly phoneNumber: string;
 }
